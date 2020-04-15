77bd94f27356a078c8be4d1df03d7af77bdae974
Ruslan Khalitov <ruslan2439@gmail.com>
2020-04-16 01:34:03 +0300

    create discriminator and generator

---
+++ GnD.py
@@ -0,0 +1,111 @@
+from torch import nn, optim
+import torch.nn.functional as F
+from torch.nn.utils import spectral_norm
+from scipy.stats import truncnorm
+
+# ----------------------------------------------------------------------------
+# Pixelwise feature vector normalization.
+# Source: https://github.com/tkarras/progressive_growing_of_gans/blob/master/networks.py#L120
+# ----------------------------------------------------------------------------
+class PixelwiseNorm(nn.Module):
+    def __init__(self):
+        super(PixelwiseNorm, self).__init__()
+
+    def forward(self, x, alpha=1e-7):
+        """
+        forward pass of the module
+        :param x: input activations volume
+        :param alpha: small number for numerical stability
+        :return: y => pixel normalized activations
+        """
+        y = x.pow(2.).mean(dim=1, keepdim=True).add(alpha).sqrt()
+        y = x / y 
+        return y
+        
+# ----------------------------------------------------------------------------
+# Discriminator and Generator classes for the GAN
+# ----------------------------------------------------------------------------
+
+class Generator(nn.Module):
+    def __init__(self, nz, nfeats, nchannels):
+        super(Generator, self).__init__()
+
+        # input is Z, going into a convolution
+        self.conv1 = spectral_norm(nn.ConvTranspose2d(nz, nfeats * 8, 4, 1, 0, bias=False))
+        #self.bn1 = nn.BatchNorm2d(nfeats * 8)
+        # state size. (nfeats*8) x 4 x 4
+        
+        self.conv2 = spectral_norm(nn.ConvTranspose2d(nfeats * 8, nfeats * 8, 4, 2, 1, bias=False))
+        #self.bn2 = nn.BatchNorm2d(nfeats * 8)
+        # state size. (nfeats*8) x 8 x 8
+        
+        self.conv3 = spectral_norm(nn.ConvTranspose2d(nfeats * 8, nfeats * 4, 4, 2, 1, bias=False))
+        #self.bn3 = nn.BatchNorm2d(nfeats * 4)
+        # state size. (nfeats*4) x 16 x 16
+        
+        self.conv4 = spectral_norm(nn.ConvTranspose2d(nfeats * 4, nfeats * 2, 4, 2, 1, bias=False))
+        #self.bn4 = nn.BatchNorm2d(nfeats * 2)
+        # state size. (nfeats * 2) x 32 x 32
+        
+        self.conv5 = spectral_norm(nn.ConvTranspose2d(nfeats * 2, nfeats, 4, 2, 1, bias=False))
+        #self.bn5 = nn.BatchNorm2d(nfeats)
+        # state size. (nfeats) x 64 x 64
+        
+        self.conv6 = spectral_norm(nn.ConvTranspose2d(nfeats, nchannels, 3, 1, 1, bias=False))
+        # state size. (nchannels) x 64 x 64
+        self.pixnorm = PixelwiseNorm()
+        
+    def forward(self, x):
+        #x = F.leaky_relu(self.bn1(self.conv1(x)))
+        #x = F.leaky_relu(self.bn2(self.conv2(x)))
+        #x = F.leaky_relu(self.bn3(self.conv3(x)))
+        #x = F.leaky_relu(self.bn4(self.conv4(x)))
+        #x = F.leaky_relu(self.bn5(self.conv5(x)))
+        x = F.leaky_relu(self.conv1(x))
+        x = F.leaky_relu(self.conv2(x))
+        x = self.pixnorm(x)
+        x = F.leaky_relu(self.conv3(x))
+        x = self.pixnorm(x)
+        x = F.leaky_relu(self.conv4(x))
+        x = self.pixnorm(x)
+        x = F.leaky_relu(self.conv5(x))
+        x = self.pixnorm(x)
+        x = torch.tanh(self.conv6(x))
+        
+        return x
+
+
+
+class Discriminator(nn.Module):
+    def __init__(self, nchannels, nfeats):
+        super(Discriminator, self).__init__()
+
+        # input is (nchannels) x 64 x 64
+        self.conv1 = nn.Conv2d(nchannels, nfeats, 4, 2, 1, bias=False)
+        
+        # state size. (nfeats) x 32 x 32 
+        self.conv2 = spectral_norm(nn.Conv2d(nfeats, nfeats * 2, 4, 2, 1, bias=False))
+        self.bn2 = nn.BatchNorm2d(nfeats * 2)
+        
+        # state size. (nfeats*2) x 16 x 16
+        self.conv3 = spectral_norm(nn.Conv2d(nfeats * 2, nfeats * 4, 4, 2, 1, bias=False))
+        self.bn3 = nn.BatchNorm2d(nfeats * 4)
+        
+        # state size. (nfeats*4) x 8 x 8
+        self.conv4 = spectral_norm(nn.Conv2d(nfeats * 4, nfeats * 8, 4, 2, 1, bias=False))
+        self.bn4 = nn.MaxPool2d(2)
+        
+        # state size. (nfeats*8) x 4 x 4
+        self.batch_discriminator = MinibatchStdDev()
+        self.pixnorm = PixelwiseNorm()
+        self.conv5 = spectral_norm(nn.Conv2d(nfeats * 8 + 1, 1, 2, 1, 0, bias=False))
+        # the final state size. 1 x 1 x 1
+        
+    def forward(self, x):
+        x = F.leaky_relu(self.conv1(x), 0.2)
+        x = F.leaky_relu(self.bn2(self.conv2(x)), 0.2)
+        x = F.leaky_relu(self.bn3(self.conv3(x)), 0.2)
+        x = F.leaky_relu(self.bn4(self.conv4(x)), 0.2)
+        x = self.batch_discriminator(x)
+        x = torch.sigmoid(self.conv5(x))
+        return x.view(-1, 1)
