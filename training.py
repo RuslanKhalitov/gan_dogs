2ac56424231fb5b3b5df3694ea148a2b4dfe951d
Ruslan Khalitov <ruslan2439@gmail.com>
2020-04-19 00:48:08 +0300

    Create main training file

---
+++ training.py
@@ -0,0 +1,107 @@
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+lr = 0.0006
+lr_d = 0.0002
+beta1 = 0.5
+epochs = 1700
+netG = Generator(100, 32, 3).to(device)
+netD = Discriminator(3, 48).to(device)
+
+criterion = nn.BCELoss()
+#criterion = nn.MSELoss()
+
+optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=lr_d, betas=(beta1, 0.999))
+lr_schedulerG = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizerG,
+                                                                     T_0=epochs//200, eta_min=0.00005)
+lr_schedulerD = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizerD,
+                                                                     T_0=epochs//200, eta_min=0.00005)
+
+nz = 100
+fixed_noise = torch.randn(25, nz, 1, 1, device=device)
+
+real_label = 0.75
+fake_label = 0.0
+batch_size = train_loader.batch_size
+
+step = 0
+for epoch in range(epochs):
+    for ii, (real_images) in tqdm(enumerate(train_loader), total=len(train_loader)):
+        end = time()
+        if (end -start) > 31000 :
+            break
+        ############################
+        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        ###########################
+        # train with real
+        netD.zero_grad()
+        real_images = real_images.to(device)
+        batch_size = real_images.size(0)
+        labels = torch.full((batch_size, 1), real_label, device=device) +  np.random.uniform(-0.1, 0.1)
+
+        output = netD(real_images)
+        errD_real = criterion(output, labels)
+        errD_real.backward()
+        D_x = output.mean().item()
+
+        # train with fake
+        noise = torch.randn(batch_size, nz, 1, 1, device=device)
+        fake = netG(noise)
+        labels.fill_(fake_label) + np.random.uniform(0, 0.2)
+        output = netD(fake.detach())
+        errD_fake = criterion(output, labels)
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+        errD = errD_real + errD_fake
+        optimizerD.step()
+
+        ############################
+        # (2) Update G network: maximize log(D(G(z)))
+        ###########################
+        netG.zero_grad()
+        labels.fill_(real_label)  # fake labels are real for generator cost
+        output = netD(fake)
+        errG = criterion(output, labels)
+        errG.backward()
+        D_G_z2 = output.mean().item()
+        optimizerG.step()
+        
+        if step % 500 == 0:
+            print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
+                  % (epoch + 1, epochs, ii, len(train_loader),
+                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+            
+            valid_image = netG(fixed_noise)
+        step += 1
+        lr_schedulerG.step(epoch)
+        lr_schedulerD.step(epoch)
+
+    if epoch % 200 == 0:
+        show_generated_img()
+        
+# torch.save(netG.state_dict(), 'generator.pth')
+# torch.save(netD.state_dict(), 'discriminator.pth')
+
+def truncated_normal(size, threshold=1):
+    values = truncnorm.rvs(-threshold, threshold, size=size)
+    return values
+
+if not os.path.exists('../output_images'):
+    os.mkdir('../output_images')
+im_batch_size = 100
+n_images=10000
+for i_batch in range(0, n_images, im_batch_size):
+    z = truncated_normal((im_batch_size, 100, 1, 1), threshold=1)
+    gen_z = torch.from_numpy(z).float().to(device)    
+    #gen_z = torch.randn(im_batch_size, 100, 1, 1, device=device)
+    gen_images = netG(gen_z)
+    images = gen_images.to("cpu").clone().detach()
+    images = images.numpy().transpose(0, 2, 3, 1)
+    for i_image in range(gen_images.size(0)):
+        save_image((gen_images[i_image, :, :, :] +1.0)/2.0, os.path.join('../output_images', f'image_{i_batch+i_image:05d}.png'))
+
+
+import shutil
+shutil.make_archive('images', 'zip', '../output_images')
